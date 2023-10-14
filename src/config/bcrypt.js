9be076dac90041ad3a244ed01fde9ca36ead8c16
Miguel Raad <miguelraad2020@gmail.com>
2023-10-14 14:59:31 -0300

    Guardando cambios antes del rebase

--- src/config/bcrypt.js
+++ src/config/bcrypt.js
@@ -1,6 +1,4 @@
 import bcrypt from 'bcrypt';
 
-export const createHash = password =>
-	bcrypt.hashSync(password, bcrypt.genSaltSync(12));
-export const isValidPassword = (password, hashPassword) =>
-	bcrypt.compareSync(password, hashPassword);
+export const createHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(12));
+export const isValidPassword = (password, hashPassword) => bcrypt.compareSync(password, hashPassword);
