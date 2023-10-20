2ba597fc2c64a8f3e476dd940599ad9100fd5f3d
Miguel Raad <miguelraad2020@gmail.com>
2023-10-20 16:52:07 -0300

    encontrando error multer

--- src/middlewares/autenticacion.js
+++ src/middlewares/autenticacion.js
@@ -2,6 +2,7 @@ import { UnauthorizedError } from "../errors/errors.js"
 
 export function autenticacion(req, res, next){
     if(!req.user){ 
+        req.logger.info("pasa por autenticación")
         req.logger.error(new UnauthorizedError())
         next(new UnauthorizedError())
     }else{
